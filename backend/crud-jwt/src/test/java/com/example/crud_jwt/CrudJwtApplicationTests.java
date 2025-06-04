089956f0d5a738e75102240aff4701fa23577405
carlosJavierRodriguez <carlosjavi1887@gmail.com>
2025-06-04 07:03:55 -0500

    first commit

---
+++ backend/crud-jwt/src/test/java/com/example/crud_jwt/CrudJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
